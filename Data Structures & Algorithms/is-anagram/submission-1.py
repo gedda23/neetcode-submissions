a6eab512d18fcb9de57c6690470bd7d794656936
Aditya Gedda <saiaditya.gedda@gmail.com>
2026-05-29 22:47:59 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        seen_dict = {}
+        if len(s) != len(t):
+            return False
+        else:
+            for char in s:
+                seen_dict[char] = seen_dict.get(char,0)+1
+            
+            for char in t:
+                seen_dict[char] = seen_dict.get(char,0)-1
+        return all(value==0for value in seen_dict.values())
